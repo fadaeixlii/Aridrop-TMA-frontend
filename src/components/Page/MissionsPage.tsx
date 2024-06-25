748512a5fbeb3b141c106ec4c284bbf2954f3085
fadaeixlii <mohammad4252@gmail.com>
2024-06-25 16:44:40 +0330

    Change ui in missions page

--- src/components/Page/MissionsPage.tsx
+++ src/components/Page/MissionsPage.tsx
@@ -11,6 +11,9 @@ import { RanksListTask } from "components/Missions/RanksListTask";
 import { ReferralRewardsListTask } from "components/Missions/ReferralRewardsListTask";
 import OPA from "assets/OPA.svg";
 import { formatCurrency } from "utils/number";
+import { useMemo } from "react";
+import { RankInfo, getUserRankInfo } from "utils/constant";
+import { PiArrowRight } from "react-icons/pi";
 
 interface Props {
   back: () => void;
@@ -19,24 +22,34 @@ interface Props {
 const MissionsPage: React.FC<Props> = ({ back }) => {
   const { tasks, loading: loadingTask } = useTaskStore();
   const { userInfo } = useUserInfo();
+
+  const userRank: RankInfo = useMemo(
+    () => getUserRankInfo(userInfo?.storedScore || 0),
+    [userInfo?.storedScore]
+  );
+
   if (!userInfo) return null;
 
   return (
     <div className="h-full flex flex-col items-center justify-start mt-4 gap-4 text-white pb-28">
-      <div className=" p-4">
-        <TelegramProfileImage
-          telegramId={userInfo?.telegramId.toString() ?? ""}
-          className="size-28"
+      <div className="flex w-full justify-center items-center flex-col gap-4   ">
+        <img
+          src={userRank.icon}
+          alt=""
+          className="aspect-square w-[50%] shrink "
         />
-        <div className="flex items-center gap-3 flex-wrap justify-center my-2">
-          <div className="font-bold">{`${userInfo?.firstName ?? ""} ${
-            userInfo?.lastName ?? ""
-          }`}</div>
-        </div>
         <div className="text-center font-extralight text-4xl py-4   flex items-center gap-1 justify-center">
           <span>{formatCurrency(userInfo.storedScore)}</span>
           <img src={OPA} alt="" className="size-8" />
         </div>
+        <span className="flex items-center gap-2 text-slate-400 text-lg">
+          <span>{userRank.key}</span>
+          <PiArrowRight
+            onClick={() => {
+              // setActiveTab("Ranks");
+            }}
+          />
+        </span>
       </div>
       <div className="grow w-full overflow-scroll flex flex-col gap-2">
         <Tabs defaultValue="special" className="">
